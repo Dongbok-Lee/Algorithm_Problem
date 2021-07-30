870e6f8181579cb568a21bdfd814af1ab5518638
dongbok Lee <typoon0820@gmail.com>
2021-07-30 23:11:01 +0900

    Create 10819.cpp
    //2021-07-30 solved

---
+++ 10819.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<algorithm>
+#include<cstdlib>
+
+using namespace std;
+
+int N;
+int arr[10];
+int result = 0;
+
+int main(){
+
+    cin >> N;
+
+    for(int i = 0; i < N; i++) cin >> arr[i];
+
+    sort(arr,arr+N);
+
+    do{
+        int x = 0;
+        for(int i = 0; i < N-1; i++) x += abs(arr[i]-arr[i+1]);
+        result = max(x, result);
+    }while(next_permutation(arr,arr+N));
+
+    cout << result;
+
+    return 0;
+}
+//2021-07-30 solved
