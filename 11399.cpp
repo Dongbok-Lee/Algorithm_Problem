a57874cd87cc53fa88ecccfc45d48180e46ca61b
dongbok Lee <typoon0820@gmail.com>
2021-07-30 23:16:05 +0900

    Create 11399.cpp
    //2021-07-28 solved

---
+++ 11399.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+int time[1001];
+int cnt = 0;
+int result = 0;
+
+int main(){
+    
+    cin >> N;
+
+    for(int i = 0; i < N; i++) cin >> time[i];
+
+    sort(time,time+N);
+
+    for(int i = 0; i < N; i++){
+
+        result += cnt + time[i];
+        cnt += time[i];
+    }
+
+    cout << result;
+
+    return 0;
+}
+//2021-07-27 solved
