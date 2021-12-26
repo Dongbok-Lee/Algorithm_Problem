4a823429c4e3a914e39ca9cc1f903ca170e30ae9
dongbok Lee <typoon0820@gmail.com>
2021-12-26 11:14:41 +0900

    Create 1259.cpp
    2021-12-26 solved

---
+++ 1259.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+void solve();
+
+void solve() {
+	string tmp;
+	while (1) {
+		cin >> tmp;
+		if (tmp == "0") {
+			break;
+		}
+		string tmp2;
+
+		for (int i = tmp.length()-1; i >= 0; i--) {
+			tmp2  += tmp[i];
+		}
+		if (!tmp2.compare(tmp)) {
+			cout << "yes" << "\n";
+		}
+		else {
+			cout << "no" << "\n";
+		}
+	}
+}
+
+int main() {
+	solve();
+	return 0;
+}//2021-12-26 solved
