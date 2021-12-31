0c25833e3957d38ddcb692d26e0e54bccbae5bbe
dongbok Lee <typoon0820@gmail.com>
2022-01-01 00:36:03 +0900

    Create 11050.cpp
    2021-12-30 solved

---
+++ 11050.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+
+int N, K;
+
+void init();
+void solve();
+int factorial(int);
+
+void init() {
+	cin >> N >> K;
+}
+
+void solve() {
+	cout << factorial(N) / (factorial(K) * factorial(N - K));
+}
+
+int factorial(int n) {
+	if (n == 1 || n == 0) {
+		return 1;
+	}
+	else {
+		return(factorial(n - 1) * n);
+	}
+}
+
+int main() {
+	init();
+	solve();
+	return 0;
+}//2021-12-30 solved
