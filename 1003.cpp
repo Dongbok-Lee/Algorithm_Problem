a8506e9881e34802987b02f0dc0a2aae2d40fc21
dongbok Lee <typoon0820@gmail.com>
2022-01-01 00:37:33 +0900

    Create 1003.cpp
    2022-01-01 solved

---
+++ 1003.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int arr[41][2] = { {1, 0}, {0, 1}, };
+int tcase;
+int N;
+
+void solve();
+
+void solve() {
+	cin >> tcase;
+
+	for (int j = 2; j < 41; j++) {
+		arr[j][0] = arr[j - 1][0] + arr[j - 2][0];
+		arr[j][1] = arr[j - 1][1] + arr[j - 2][1];
+	}
+	
+	for (int i = 0; i < tcase; i++) {
+		cin >> N;
+		cout << arr[N][0] << " " << arr[N][1] << "\n";
+	}
+}
+
+int main() {
+	solve();
+	return 0;
+}//2022-01-01 solved
