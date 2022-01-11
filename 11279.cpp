b08ebe817ed32f3a8ee3efccd2e33538978bc0ce
dongbok Lee <typoon0820@gmail.com>
2022-01-11 15:32:39 +0900

    Create 11279.cpp
    2022-01-10 solved

---
+++ 11279.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <vector>
+#include <functional>
+
+using namespace std;
+
+int N;
+priority_queue<int, vector<int>> q;
+
+void init();
+void solve();
+
+void init() {
+	cin >> N;
+}
+
+void solve() {
+	int tmp;
+	for (int i = 0; i < N; i++) {
+		cin >> tmp;
+		if (!tmp && !q.empty()) {
+			cout << q.top() << '\n';
+			q.pop();
+		}
+		else if (!tmp && q.empty()) {
+			cout << 0 << '\n';
+		}
+		else if(tmp){
+			q.push(tmp);
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	init();
+	solve();
+}//2022-01-10 solved
