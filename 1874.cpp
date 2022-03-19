f8c12631b8c8c5ab27f287d6ecf7dc8a85579064
dongbok Lee <typoon0820@gmail.com>
2022-03-19 15:54:12 +0900

    Create 1874.cpp
    2022-03-19 solved

---
+++ 1874.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int N, cnt = 1;
+stack<int> s;
+string pm;
+void init();
+void solve();
+
+void init() {
+	cin >> N;
+}
+
+void solve() {
+	for(int i = 0; i < N; i ++){
+		int num;
+		cin >> num;
+
+		if (!s.empty() && num <= s.top()) {
+			if (num == s.top()) {
+				s.pop();
+				pm += "-";
+			}
+			else {
+				cout << "NO\n";
+				return;
+			}
+		}
+		else {
+			while (cnt != num+1) {
+				s.push(cnt);
+				cnt++;
+				pm += "+";
+			}
+			s.pop();
+			pm += "-";
+		}
+	}
+
+	for (int i = 0; i < pm.size(); i++) {
+		cout << pm[i] << "\n";
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	init();
+	solve();
+
+	return 0;
+}//2022-03-19 solved
