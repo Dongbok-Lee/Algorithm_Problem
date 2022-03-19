6cd3ed9e6e11b9c493ef2320f53f5ab1f043f81d
dongbok Lee <typoon0820@gmail.com>
2022-03-19 21:35:02 +0900

    Create 10773.cpp
    2022-03-19 solved

---
+++ 10773.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<stack>
+#include<algorithm>
+#include<cmath>
+#include<vector>
+
+using namespace std;
+
+stack<int> s;
+int N;
+
+void solve();
+
+void solve() {
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int num;
+		cin >> num;
+
+		if (num == 0)
+			s.pop();
+		else
+			s.push(num);
+	}
+	int sum = 0;
+	for (int i = 0; i < s.size(); i++) {
+		while (!s.empty()) {
+			sum += s.top();
+			s.pop();
+		}
+	}
+	cout << sum;
+}
+
+int main() {
+	solve();
+	return 0;
+}//2022-03-19 solved
