3f1fe12bf42f78f6f2326aaa273a290a10de36c8
dongbok Lee <typoon0820@gmail.com>
2022-03-19 21:36:59 +0900

    Create 2231.cpp
    2022-03-19 solved

---
+++ 2231.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+#include<queue>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+
+void init();
+void solve();
+
+int N;
+
+void init() {
+	cin >> N;
+}
+
+void solve() {
+	for (int i = 1; i < N; i++) {
+		int num = i;
+		int result = i;
+		
+		while (num != 0) {
+			result += num % 10;
+			num /= 10;
+		}
+			
+		
+		if (result == N) {
+			cout << i;
+			return;
+		}
+			
+	}
+	cout << 0;
+}
+
+int main() {
+	init();
+	solve();
+
+	return 0;
+}//2#include<iostream>
+#include<queue>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+
+void init();
+void solve();
+
+int N;
+
+void init() {
+	cin >> N;
+}
+
+void solve() {
+	for (int i = 1; i < N; i++) {
+		int num = i;
+		int result = i;
+		
+		while (num != 0) {
+			result += num % 10;
+			num /= 10;
+		}
+			
+		
+		if (result == N) {
+			cout << i;
+			return;
+		}
+			
+	}
+	cout << 0;
+}
+
+int main() {
+	init();
+	solve();
+
+	return 0;
+}//2022-03-19 solved
