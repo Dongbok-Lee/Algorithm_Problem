ac01bdfd158b4c5fe4102873a41b0c94088112c6
dongbok Lee <typoon0820@gmail.com>
2022-07-11 21:21:15 +0900

    [Bronze V] Title: 개수 세기, Time: 240 ms, Memory: 17864 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+    	
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        int[] arr = new int[201];
+        for (int i = 0; i<N; i++)
+            arr[sc.nextInt()+100]++;
+        System.out.print(arr[sc.nextInt()+100]);
+    }
+}
