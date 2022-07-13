1a563d3abc4bb3553de05a99f8ec2517a8bf414a
dongbok Lee <typoon0820@gmail.com>
2022-07-13 18:32:07 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 232 ms, Memory: 18024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+
+    static HashMap<String, Double> map = new HashMap<String, Double>();
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] ars) {
+        while(sc.hasNext()){
+            System.out.println(sc.nextLine());
+        }
+    }
+}
+
