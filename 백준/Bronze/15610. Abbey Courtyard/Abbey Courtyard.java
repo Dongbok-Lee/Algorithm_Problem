57201ae3a84c2badc9ddb15846de20130a214fc5
dongbok Lee <typoon0820@gmail.com>
2022-07-16 16:56:17 +0900

    [Bronze IV] Title: Abbey Courtyard, Time: 240 ms, Memory: 18008 KB -BaekjoonHub

---
+++ 백준/Bronze/15610. Abbey Courtyard/Abbey Courtyard.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] ars) {
+        double square = sc.nextDouble();
+        double cost = Math.sqrt(square)*4;
+        System.out.println(cost);
+    }
+}
