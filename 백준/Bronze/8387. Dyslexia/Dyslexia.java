0a2372592545189ff3c97f74a7031936243c3882
dongbok Lee <typoon0820@gmail.com>
2022-07-17 06:02:08 +0900

    [Bronze II] Title: Dyslexia, Time: 256 ms, Memory: 18236 KB -BaekjoonHub

---
+++ 백준/Bronze/8387. Dyslexia/Dyslexia.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+    static int N;
+    static int count = 0;
+    static String str, str2;
+
+    public static void main(String[] ars) {
+        N = sc.nextInt();
+        sc.nextLine();
+
+        str = sc.nextLine();
+        str2 = sc.nextLine();
+
+        for(int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == str2.charAt(i))
+                count++;
+        }
+        System.out.println(count);
+    }
+}
