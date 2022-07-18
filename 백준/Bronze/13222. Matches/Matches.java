f37f631b7d93bc72e603ea3914d7e4e8f283e90d
dongbok Lee <typoon0820@gmail.com>
2022-07-19 07:21:16 +0900

    [Bronze III] Title: Matches, Time: 116 ms, Memory: 12920 KB -BaekjoonHub

---
+++ 백준/Bronze/13222. Matches/Matches.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+    static int N;
+    static int W;
+    static int H;
+    public static void main(String[] ars) {
+        N = sc.nextInt();
+        W = sc.nextInt();
+        H = sc.nextInt();
+
+        double MAX = Math.sqrt(W*W + H*H);
+
+        for(int i = 0; i < N; i++) {
+            int length = sc.nextInt();
+            if(length > MAX)
+                System.out.println("NO");
+            else
+                System.out.println("YES");
+        }
+    }
+}
