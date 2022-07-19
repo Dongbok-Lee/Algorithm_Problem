eeea6c7784e1aa06aa6570ecf02ab78d18d7ea96
dongbok Lee <typoon0820@gmail.com>
2022-07-20 02:40:28 +0900

    [Bronze V] Title: ??!, Time: 108 ms, Memory: 12988 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] ars) throws IOException {
+       String str = sc.nextLine();
+       System.out.println(str + "??!");
+    }
+}
