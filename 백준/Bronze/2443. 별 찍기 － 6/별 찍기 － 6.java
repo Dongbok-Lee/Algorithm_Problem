74ade0c0d6d5277b6d27a7d7078be3d0444860b4
dongbok Lee <typoon0820@gmail.com>
2022-07-21 12:47:47 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 328 ms, Memory: 18088 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int N;
+    public static void main(String[] ars) throws IOException {
+
+        N = sc.nextInt();
+
+        for(int i = 0; i < N; i++){
+            for(int j = N-i; j < N; j++)
+                System.out.print(" ");
+            for(int j = i*2; j < N*2-1; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
