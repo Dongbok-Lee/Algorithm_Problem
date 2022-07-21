9354cd129d0368c1a7bba44c5d10366fc0f85660
dongbok Lee <typoon0820@gmail.com>
2022-07-21 12:35:06 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 100 ms, Memory: 12804 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] ars) throws IOException {
+        System.out.println("고려대학교");
+    }
+}
