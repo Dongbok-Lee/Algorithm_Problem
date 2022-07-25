cf5aa61d5629be160c9f08972d4fe75f4c084288
dongbok Lee <typoon0820@gmail.com>
2022-07-25 15:04:42 +0900

    [Bronze V] Title: 웰컴, Time: 108 ms, Memory: 12744 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int T;
+
+    public static void main(String[] ars) throws IOException {
+        System.out.println(".  .   .\n" + "|  | _ | _. _ ._ _  _\n" + "|/\\|(/.|(_.(_)[ | )(/.\n");
+    }
+}
