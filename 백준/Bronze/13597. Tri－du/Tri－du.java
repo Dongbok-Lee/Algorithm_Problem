ba88bee3531e5554aa7ba7742ca8ef0c3e34666c
dongbok Lee <typoon0820@gmail.com>
2022-09-12 00:20:24 +0900

    [Bronze IV] Title: Tri-du, Time: 112 ms, Memory: 13016 KB -BaekjoonHub

---
+++ 백준/Bronze/13597. Tri－du/Tri－du.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        System.out.println(Math.max(sc.nextInt(), sc.nextInt()));
+    }
+}
+
+
