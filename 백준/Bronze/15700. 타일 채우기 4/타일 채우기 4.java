25fe08d7becb25311d89e0eb1aaed84acf37f746
dongbok Lee <typoon0820@gmail.com>
2022-09-12 00:28:01 +0900

    [Bronze IV] Title: 타일 채우기 4, Time: 112 ms, Memory: 12900 KB -BaekjoonHub

---
+++ 백준/Bronze/15700. 타일 채우기 4/타일 채우기 4.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        System.out.println((sc.nextLong() * sc.nextLong()) / 2);
+    }
+}
