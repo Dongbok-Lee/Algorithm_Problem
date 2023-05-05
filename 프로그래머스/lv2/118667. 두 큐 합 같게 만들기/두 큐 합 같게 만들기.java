db5939238e45ad8a48e22ea02adf4847be456457
dongbok Lee <typoon0820@gmail.com>
2023-05-06 02:57:33 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 54.76 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -9,45 +9,11 @@ class Solution {
         long q1Sum = 0;
         long q2Sum = 0;
         
-        int answer = 0;
-        
         for(int i = 0; i < queue1.length; i++){
-            
-            q1Sum += queue1[i];
-            q2Sum += queue2[i];
-            
-            q1.offer(queue1[i]);
-            q2.offer(queue2[i]);
         }
         
-        int qSize = q1.size() * 2;
         
-        while(q1Sum != q2Sum){
-            
-            answer++;
-            int num = -1;
-            
-            if(q1Sum > q2Sum){
-                num = q1.poll();
-                q2.offer(num);
-                q1Sum -= num;
-                q2Sum += num;
-            }else{
-                num = q2.poll();
-                q1.offer(num);
-                q1Sum += num;
-                q2Sum -= num;
-            }
-            
-            if(qSize == -10){
-                answer = -1;
-                break;
-            }
-            
-            qSize--;
-            
-        }
-        
+        int answer = -2;
         return answer;
     }
 }
