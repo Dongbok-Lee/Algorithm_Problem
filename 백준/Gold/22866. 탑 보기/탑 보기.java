6480aeeeb83c224b626f37073d4082bd8dc85b33
dongbok Lee <typoon0820@gmail.com>
2023-12-27 13:58:23 +0900

    [Gold III] Title: 탑 보기, Time: 1972 ms, Memory: 63152 KB -BaekjoonHub

---
+++ 백준/Gold/22866. 탑 보기/탑 보기.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N;
+    static int[] arr;
+    static int[] buildingNumber;
+    static int[] closeBuilding;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+
+        arr = new int[N];
+        buildingNumber = new int[N];
+        closeBuilding = new int[N];
+
+        Arrays.fill(closeBuilding, 987654321);
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Stack<Building> s = new Stack<>();
+
+        for(int i = 0; i < N; i++) {
+
+            while(!s.empty() && s.peek().height <= arr[i]) {
+                s.pop();
+            }
+
+            buildingNumber[i] += s.size();
+
+            if(!s.empty()){
+                closeBuilding[i] = s.peek().index;
+            }
+
+            s.push(new Building(arr[i], i));
+        }
+
+        s = new Stack<>();
+
+        for (int i = N - 1; i >= 0; i--) {
+
+            while(!s.empty() && s.peek().height <= arr[i]) {
+                s.pop();
+            }
+
+            buildingNumber[i] += s.size();
+
+            if(!s.empty() && (Math.abs(closeBuilding[i] - i) > Math.abs(s.peek().index - i))) {
+                    closeBuilding[i] = s.peek().index;
+            }
+
+            s.push(new Building(arr[i], i));
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (buildingNumber[i] == 0) {
+                System.out.println(0);
+            } else {
+                System.out.println(buildingNumber[i] + " " + (closeBuilding[i] + 1));
+            }
+        }
+    }
+}
+
+class Building {
+    int height, index;
+
+    public Building(int height, int index) {
+        this.height = height;
+        this.index = index;
+    }
+}
